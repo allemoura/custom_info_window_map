b6826f09ceef2b286943999c567a77b5624b98c1
Alessandra Barros <33128122+allemoura@users.noreply.github.com>
2023-07-10 17:05:56 -0300

    start project

---
+++ example/lib/main.dart
@@ -0,0 +1,142 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:clippy_flutter/triangle.dart';
+import 'package:custom_info_window_map/custom_info_window_map.dart';
+import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CustomInfoWindowExample(),
+    );
+  }
+}
+
+class CustomInfoWindowExample extends StatefulWidget {
+  const CustomInfoWindowExample({super.key});
+
+  @override
+  State<CustomInfoWindowExample> createState() =>
+      _CustomInfoWindowExampleState();
+}
+
+class _CustomInfoWindowExampleState extends State<CustomInfoWindowExample> {
+  final CustomInfoWindowMapController _customInfoWindowController =
+      CustomInfoWindowMapController();
+
+  final LatLng _latLng = const LatLng(-7.162305, -34.817195);
+  final double _zoom = 15.0;
+
+  @override
+  void dispose() {
+    _customInfoWindowController.dispose();
+    super.dispose();
+  }
+
+  final Set<Marker> _markers = {};
+
+  @override
+  Widget build(BuildContext context) {
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("marker_ci"),
+        position: _latLng,
+        onTap: () {
+          _customInfoWindowController.addInfoWindow!(
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.school,
+                            color: Colors.blue,
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Expanded(
+                            child: RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(
+                                text: "Universidade Federal da Paraíba",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                      color: Colors.black,
+                                    ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Triangle.isosceles(
+                  edge: Edge.BOTTOM,
+                  child: Container(
+                    color: Colors.white,
+                    width: 20.0,
+                    height: 10.0,
+                  ),
+                ),
+              ],
+            ),
+            _latLng,
+          );
+        },
+      ),
+    );
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onTap: (position) {
+              _customInfoWindowController.hideInfoWindow!();
+            },
+            onCameraMove: (position) {
+              _customInfoWindowController.onCameraMove!();
+            },
+            onMapCreated: (GoogleMapController controller) async {
+              _customInfoWindowController.googleMapController = controller;
+            },
+            markers: _markers,
+            initialCameraPosition: CameraPosition(
+              target: _latLng,
+              zoom: _zoom,
+            ),
+          ),
+          CustomInfoWindowMap(
+            (top, left, width, height) => null,
+            controller: _customInfoWindowController,
+            height: 90,
+            width: 150,
+            offset: 50,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/custom_info_window_map.dart
@@ -0,0 +1,150 @@
+/// A widget based custom info window for google_maps_flutter package.
+// ignore_for_file: library_private_types_in_public_api
+
+library custom_info_window;
+
+import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// Controller to add, update and control the custom info window.
+class CustomInfoWindowMapController {
+  /// Add custom [Widget] and [Marker]'s [LatLng] to [CustomInfoWindowMap] and make it visible.
+  Function(Widget, LatLng)? addInfoWindow;
+
+  /// Notifies [CustomInfoWindowMap] to redraw as per change in position.
+  VoidCallback? onCameraMove;
+
+  /// Hides [CustomInfoWindowMap].
+  VoidCallback? hideInfoWindow;
+
+  /// Shows [CustomInfoWindowMap].
+  VoidCallback? showInfoWindow;
+
+  /// Holds [GoogleMapController] for calculating [CustomInfoWindowMap] position.
+  GoogleMapController? googleMapController;
+
+  void dispose() {
+    addInfoWindow = null;
+    onCameraMove = null;
+    hideInfoWindow = null;
+    showInfoWindow = null;
+    googleMapController = null;
+  }
+}
+
+/// A stateful widget responsible to create widget based custom info window.
+class CustomInfoWindowMap extends StatefulWidget {
+  /// A [CustomInfoWindowMapController] to manipulate [CustomInfoWindowMap] state.
+  final CustomInfoWindowMapController controller;
+
+  /// Offset to maintain space between [Marker] and [CustomInfoWindowMap].
+  final double offset;
+
+  /// Height of [CustomInfoWindowMap].
+  final double height;
+
+  /// Width of [CustomInfoWindowMap].
+  final double width;
+
+  final Function(double top, double left, double width, double height) onChange;
+
+  const CustomInfoWindowMap(
+    this.onChange, {super.key, 
+    required this.controller,
+    this.offset = 50,
+    this.height = 50,
+    this.width = 100,
+  })  : assert(offset >= 0),
+        assert(height >= 0),
+        assert(width >= 0);
+
+  @override
+  _CustomInfoWindowMapState createState() => _CustomInfoWindowMapState();
+}
+
+class _CustomInfoWindowMapState extends State<CustomInfoWindowMap> {
+  bool _showNow = false;
+  double _leftMargin = 0;
+  double _topMargin = 0;
+  Widget? _child;
+  LatLng? _latLng;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addInfoWindow = _addInfoWindow;
+    widget.controller.onCameraMove = _onCameraMove;
+    widget.controller.hideInfoWindow = _hideInfoWindow;
+    widget.controller.showInfoWindow = _showInfoWindow;
+  }
+
+  /// Calculate the position on [CustomInfoWindowMap] and redraw on screen.
+  void _updateInfoWindow() async {
+    if (_latLng == null ||
+        _child == null ||
+        widget.controller.googleMapController == null) {
+      return;
+    }
+    ScreenCoordinate screenCoordinate = await widget
+        .controller.googleMapController!
+        .getScreenCoordinate(_latLng!);
+    double devicePixelRatio = 1.0;
+    double left =
+        (screenCoordinate.x.toDouble() / devicePixelRatio) - (widget.width / 2);
+    double top = (screenCoordinate.y.toDouble() / devicePixelRatio) -
+        (widget.offset + widget.height);
+    setState(() {
+      _showNow = true;
+      _leftMargin = left;
+      _topMargin = top;
+    });
+    widget.onChange.call(top, left, widget.width, widget.height);
+  }
+
+  /// Assign the [Widget] and [Marker]'s [LatLng].
+  void _addInfoWindow(Widget child, LatLng latLng) {
+    _child = child;
+    _latLng = latLng;
+    _updateInfoWindow();
+  }
+
+  /// Notifies camera movements on [GoogleMap].
+  void _onCameraMove() {
+    if (!_showNow) return;
+    _updateInfoWindow();
+  }
+
+  /// Disables [CustomInfoWindowMap] visibility.
+  void _hideInfoWindow() {
+    setState(() {
+      _showNow = false;
+    });
+  }
+
+  /// Enables [CustomInfoWindowMap] visibility.
+  void _showInfoWindow() {
+    _updateInfoWindow();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: _leftMargin,
+      top: _topMargin,
+      child: Visibility(
+        visible: (_showNow == false ||
+                (_leftMargin == 0 && _topMargin == 0) ||
+                _child == null ||
+                _latLng == null)
+            ? false
+            : true,
+        child: SizedBox(
+          height: widget.height,
+          width: widget.width,
+          child: _child,
+        ),
+      ),
+    );
+  }
+}
